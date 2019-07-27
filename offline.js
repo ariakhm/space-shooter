051eda8fc03ac7a0b03412213e55ff27794ccf0d
ariakhm <ariakhosravi84@gmail.com>
2019-07-27 09:08:16 +0430

    uploaad

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1564202045,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/enred-sheet0.png",
+		"images/enblue-sheet0.png",
+		"images/engreen-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite3-sheet0.png",
+		"images/uiup.png",
+		"images/redline-sheet0.png",
+		"images/planet-sheet0.png",
+		"images/ufored-sheet0.png",
+		"media/sfx_laser1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
